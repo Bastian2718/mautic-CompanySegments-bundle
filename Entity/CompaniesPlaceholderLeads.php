cd4eec93785274bc34692d8c6d9c096f6adcb63a
Jonas <j.ludwig@leuchtfeuer.com>
2026-01-21 11:32:09 +0100

    MTC-8820 M6 Introduce placeholder contacts

---
+++ Entity/CompaniesPlaceholderLeads.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MauticPlugin\LeuchtfeuerCompanySegmentsBundle\Entity;
+
+use Doctrine\ORM\Mapping\ClassMetadata as ORMClassMetadata;
+use Mautic\CoreBundle\Doctrine\Mapping\ClassMetadataBuilder;
+use Mautic\LeadBundle\Entity\Company;
+use Mautic\LeadBundle\Entity\Lead;
+
+class CompaniesPlaceholderLeads
+{
+    public const TABLE_NAME     = 'companies_placeholder_leads';
+    public const RELATIONS_NAME = 'cspl';
+
+    private Company $company;
+    private Lead $lead;
+
+    public static function loadMetadata(ORMClassMetadata $metadata): void
+    {
+        $builder = new ClassMetadataBuilder($metadata);
+
+        $builder->setTable(self::TABLE_NAME)
+            ->setCustomRepositoryClass(CompaniesPlaceholderLeadsRepository::class);
+
+        $builder->createManyToOne('company', Company::class)
+            ->isPrimaryKey()
+            ->addJoinColumn('company_id', 'id', false, false, 'CASCADE')
+            ->cascadeRefresh()
+            ->build();
+
+        $builder->createManyToOne('lead', Lead::class)
+            ->addJoinColumn('lead_id', 'id', false, false, 'CASCADE')
+            ->build();
+    }
+
+    public function getCompany(): Company
+    {
+        return $this->company;
+    }
+
+    public function setCompany(Company $company): void
+    {
+        $this->company = $company;
+    }
+
+    public function getLead(): Lead
+    {
+        return $this->lead;
+    }
+
+    public function setLead(Lead $lead): void
+    {
+        $this->lead = $lead;
+    }
+}
