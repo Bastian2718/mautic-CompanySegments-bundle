11bbaa697f44f406d34d91a40ae62f96e0b77fb7
Jonas <j.ludwig@leuchtfeuer.com>
2026-02-05 10:11:20 +0100

    9775 Add form

---
+++ Form/Extension/CompanyTypeExtension.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MauticPlugin\LeuchtfeuerCompanySegmentsBundle\Form\Extension;
+
+use Mautic\LeadBundle\Entity\Company;
+use Mautic\LeadBundle\Form\Type\CompanyType;
+use MauticPlugin\LeuchtfeuerCompanySegmentsBundle\Entity\CompaniesSegmentsRepository;
+use MauticPlugin\LeuchtfeuerCompanySegmentsBundle\Form\Type\CompanySegmentListType;
+use MauticPlugin\LeuchtfeuerCompanySegmentsBundle\Integration\Config;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class CompanyTypeExtension extends AbstractTypeExtension
+{
+    public function __construct(
+        private TranslatorInterface $translator,
+        private Config $config,
+        private CompaniesSegmentsRepository $companiesSegmentsRepository,
+    ) {
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        if (!$this->config->isPublished()) {
+            return;
+        }
+
+        $company = $options['data'] ?? null;
+        $currentSegments = [];
+
+        if ($company instanceof Company && null !== $company->getId()) {
+            $companiesSegments = $this->companiesSegmentsRepository->getByCompany($company);
+            $currentSegments = array_map(fn($cs) => $cs->getCompanySegment()->getId(), $companiesSegments);
+        }
+
+        $builder->add(
+            'company_segments',
+            CompanySegmentListType::class,
+            [
+                'label'      => 'mautic.company_segments.form.segments',
+                'label_attr' => ['class' => 'control-label'],
+                'attr'       => [
+                    'class'                         => 'form-control',
+                    'data-placeholder'              => $this->translator->trans('mautic.company_segments.form.segments.placeholder'),
+                    'data-company-segments-manual'  => 'true',
+                ],
+                'required' => false,
+                'multiple' => true,
+                'mapped'   => false,
+                'data'     => $currentSegments,
+            ]
+        );
+    }
+
+    public static function getExtendedTypes(): iterable
+    {
+        return [CompanyType::class];
+    }
+}
